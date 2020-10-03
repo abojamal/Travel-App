eb92eaa32c5747797dc401fb12c622d21ce1dfe7
shafik <shafiksharif@gmail.com>
2020-10-03 23:10:14 +0300

    docs:webpack ready

---
+++ src/client/index.js
@@ -0,0 +1,5 @@
+import { handleSubmit } from './js/app';
+document.getElementById(submit - btn).addEventListener('click', handleSubmit);
+import './styles/styles.scss';
+export { handleSubmit };
+alert('I exist!');
